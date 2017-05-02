ad6e5466d4ab7a36e0fca3c26babf529d639c799
wumin.sunland <fusu1435@163.com>
2017-05-02 18:07:44 +0800

    Add full connected neural network

---
+++ ArtificialNeuralNetwork/FullConnectedNeuralNetwork.py
@@ -0,0 +1,495 @@
+#!usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+# #  FileName    : 
+# #  Author      : Administrator
+# #  Description : 
+# #  Time        : 2017/5/2
+
+from functools import reduce
+import numpy as np
+from  numpy import random
+# from MachineLearning.utils.dataLoader import get_training_data_set
+# from MachineLearning.utils.dataLoader import get_test_data_set
+from MachineLearning.utils import DataLoader
+from datetime import datetime
+
+def sigmoid(x):
+    return 0
+
+# 节点类，负责记录和维护节点自身信息以及与这个节点相关的上下游连接，实现输出值和误差项的计算。
+class Node(object):
+    def __init__(self, layer_index, node_index):
+        '''
+        构造节点对象。
+        layer_index: 节点所属的层的编号
+        node_index: 节点的编号
+        '''
+        self.layer_index = layer_index
+        self.node_index = node_index
+        self.downstream = []
+        self.upstream = []
+        self.output = 0
+        self.delta = 0
+
+    def set_output(self, output):
+        '''
+        设置节点的输出值。如果节点属于输入层会用到这个函数。
+        '''
+        self.output = output
+
+    def append_downstream_connection(self, conn):
+        '''
+        添加一个到下游节点的连接
+        '''
+        self.downstream.append(conn)
+
+    def append_upstream_connection(self, conn):
+        '''
+        添加一个到上游节点的连接
+        '''
+        self.upstream.append(conn)
+
+    def calc_output(self):
+        '''
+        根据式1计算节点的输出
+        '''
+        output = reduce(lambda ret, conn: ret + conn.upstream_node.output * conn.weight, self.upstream, 0)
+        self.output = sigmoid(output)
+
+    def calc_hidden_layer_delta(self):
+        '''
+        节点属于隐藏层时，根据式4计算delta
+        '''
+        downstream_delta = reduce(
+            lambda ret, conn: ret + conn.downstream_node.delta * conn.weight,
+            self.downstream, 0.0)
+        self.delta = self.output * (1 - self.output) * downstream_delta
+
+    def calc_output_layer_delta(self, label):
+        '''
+        节点属于输出层时，根据式3计算delta
+        '''
+        self.delta = self.output * (1 - self.output) * (label - self.output)
+
+    def __str__(self):
+        '''
+        打印节点的信息
+        '''
+        node_str = '%u-%u: output: %f delta: %f' % (self.layer_index, self.node_index, self.output, self.delta)
+        downstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.downstream, '')
+        upstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.upstream, '')
+        return node_str + '\n\tdownstream:' + downstream_str + '\n\tupstream:' + upstream_str
+
+class ConstNode(object):
+    def __init__(self, layer_index, node_index):
+        '''
+        构造节点对象。
+        layer_index: 节点所属的层的编号
+        node_index: 节点的编号
+        '''
+        self.layer_index = layer_index
+        self.node_index = node_index
+        self.downstream = []
+        self.output = 1
+
+    def append_downstream_connection(self, conn):
+        '''
+        添加一个到下游节点的连接
+        '''
+        self.downstream.append(conn)
+
+    def calc_hidden_layer_delta(self):
+        '''
+        节点属于隐藏层时，根据式4计算delta
+        '''
+        downstream_delta = reduce(
+            lambda ret, conn: ret + conn.downstream_node.delta * conn.weight,
+            self.downstream, 0.0)
+        self.delta = self.output * (1 - self.output) * downstream_delta
+
+    def __str__(self):
+        '''
+        打印节点的信息
+        '''
+        node_str = '%u-%u: output: 1' % (self.layer_index, self.node_index)
+        downstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.downstream, '')
+        return node_str + '\n\tdownstream:' + downstream_str
+
+class Layer(object):
+    def __init__(self, layer_index, node_count):
+        '''
+        初始化一层
+        layer_index: 层编号
+        node_count: 层所包含的节点个数
+        '''
+        self.layer_index = layer_index
+        self.nodes = []
+        for i in range(node_count):
+            self.nodes.append(Node(layer_index, i))
+        self.nodes.append(ConstNode(layer_index, node_count))
+
+    def set_output(self, data):
+        '''
+        设置层的输出。当层是输入层时会用到。
+        '''
+        for i in range(len(data)):
+            self.nodes[i].set_output(data[i])
+
+    def calc_output(self):
+        '''
+        计算层的输出向量
+        '''
+        for node in self.nodes[:-1]:
+            node.calc_output()
+
+    def dump(self):
+        '''
+        打印层的信息
+        '''
+        for node in self.nodes:
+            print (node)
+
+class Connection(object):
+    def __init__(self, upstream_node, downstream_node):
+        '''
+        初始化连接，权重初始化为是一个很小的随机数
+        upstream_node: 连接的上游节点
+        downstream_node: 连接的下游节点
+        '''
+        self.upstream_node = upstream_node
+        self.downstream_node = downstream_node
+
+        self.weight = random.uniform(-0.1, 0.1)
+        self.gradient = 0.0
+
+    def calc_gradient(self):
+        '''
+        计算梯度
+        '''
+        self.gradient = self.downstream_node.delta * self.upstream_node.output
+
+    def get_gradient(self):
+        '''
+        获取当前的梯度
+        '''
+        return self.gradient
+
+    def update_weight(self, rate):
+        '''
+        根据梯度下降算法更新权重
+        '''
+        self.calc_gradient()
+        self.weight += rate * self.gradient
+
+    def __str__(self):
+        '''
+        打印连接信息
+        '''
+        return '(%u-%u) -> (%u-%u) = %f' % (
+            self.upstream_node.layer_index,
+            self.upstream_node.node_index,
+            self.downstream_node.layer_index,
+            self.downstream_node.node_index,
+            self.weight)
+
+class Connections(object):
+    def __init__(self):
+        self.connections = []
+
+    def add_connection(self, connection):
+        self.connections.append(connection)
+
+    def dump(self):
+        for conn in self.connections:
+            print (conn)
+
+class Network(object):
+    def __init__(self, layers):
+        '''
+        初始化一个全连接神经网络
+        layers: 二维数组，描述神经网络每层节点数
+        '''
+        self.connections = Connections()
+        self.layers = []
+        layer_count = len(layers)
+        node_count = 0;
+        for i in range(layer_count):
+            self.layers.append(Layer(i, layers[i]))
+        for layer in range(layer_count - 1):
+            connections = [Connection(upstream_node, downstream_node)
+                           for upstream_node in self.layers[layer].nodes
+                           for downstream_node in self.layers[layer + 1].nodes[:-1]]
+            for conn in connections:
+                self.connections.add_connection(conn)
+                conn.downstream_node.append_upstream_connection(conn)
+                conn.upstream_node.append_downstream_connection(conn)
+
+
+    def train(self, labels, data_set, rate, iteration):
+        '''
+        训练神经网络
+        labels: 数组，训练样本标签。每个元素是一个样本的标签。
+        data_set: 二维数组，训练样本特征。每个元素是一个样本的特征。
+        '''
+        for i in range(iteration):
+            for d in range(len(data_set)):
+                self.train_one_sample(labels[d], data_set[d], rate)
+
+    def train_one_sample(self, label, sample, rate):
+        '''
+        内部函数，用一个样本训练网络
+        '''
+        self.predict(sample)
+        self.calc_delta(label)
+        self.update_weight(rate)
+
+    def calc_delta(self, label):
+        '''
+        内部函数，计算每个节点的delta
+        '''
+        output_nodes = self.layers[-1].nodes
+        for i in range(len(label)):
+            output_nodes[i].calc_output_layer_delta(label[i])
+        for layer in self.layers[-2::-1]:
+            for node in layer.nodes:
+                node.calc_hidden_layer_delta()
+
+    def update_weight(self, rate):
+        '''
+        内部函数，更新每个连接权重
+        '''
+        for layer in self.layers[:-1]:
+            for node in layer.nodes:
+                for conn in node.downstream:
+                    conn.update_weight(rate)
+
+    def calc_gradient(self):
+        '''
+        内部函数，计算每个连接的梯度
+        '''
+        for layer in self.layers[:-1]:
+            for node in layer.nodes:
+                for conn in node.downstream:
+                    conn.calc_gradient()
+
+    def get_gradient(self, label, sample):
+        '''
+        获得网络在一个样本下，每个连接上的梯度
+        label: 样本标签
+        sample: 样本输入
+        '''
+        self.predict(sample)
+        self.calc_delta(label)
+        self.calc_gradient()
+
+    def predict(self, sample):
+        '''
+        根据输入的样本预测输出值
+        sample: 数组，样本的特征，也就是网络的输入向量
+        '''
+        self.layers[0].set_output(sample)
+        for i in range(1, len(self.layers)):
+            self.layers[i].calc_output()
+        return list(map(lambda node: node.output, self.layers[-1].nodes[:-1]))
+
+    def dump(self):
+        '''
+        打印网络信息
+        '''
+        for layer in self.layers:
+            layer.dump()
+def gradient_check(network, sample_feature, sample_label):
+    '''
+    梯度检查
+    network: 神经网络对象
+    sample_feature: 样本的特征
+    sample_label: 样本的标签
+    '''
+    # 计算网络误差
+    network_error = lambda vec1, vec2: \
+            0.5 * reduce(lambda a, b: a + b,
+                      map(lambda v: (v[0] - v[1]) * (v[0] - v[1]),
+                          zip(vec1, vec2)))
+
+    # 获取网络在当前样本下每个连接的梯度
+    network.get_gradient(sample_feature, sample_label)
+
+    # 对每个权重做梯度检查
+    for conn in network.connections.connections:
+        # 获取指定连接的梯度
+        actual_gradient = conn.get_gradient()
+
+        # 增加一个很小的值，计算网络的误差
+        epsilon = 0.0001
+        conn.weight += epsilon
+        error1 = network_error(network.predict(sample_feature), sample_label)
+
+        # 减去一个很小的值，计算网络的误差
+        conn.weight -= 2 * epsilon # 刚才加过了一次，因此这里需要减去2倍
+        error2 = network_error(network.predict(sample_feature), sample_label)
+
+        # 根据式6计算期望的梯度值
+        expected_gradient = (error2 - error1) / (2 * epsilon)
+
+        # 打印
+        print ('expected gradient: \t%f\nactual gradient: \t%f' % (
+            expected_gradient, actual_gradient))
+
+# 全连接层实现类
+class FullConnectedLayer(object):
+    def __init__(self, input_size, output_size,
+                 activator):
+        '''
+        构造函数
+        input_size: 本层输入向量的维度
+        output_size: 本层输出向量的维度
+        activator: 激活函数
+        '''
+        self.input_size = input_size
+        self.output_size = output_size
+        self.activator = activator
+        # 权重数组W
+        self.W = np.random.uniform(-0.1, 0.1,
+            (output_size, input_size))
+        # 偏置项b
+        self.b = np.zeros((output_size, 1))
+        # 输出向量
+        self.output = np.zeros((output_size, 1))
+
+    def forward(self, input_array):
+        '''
+        前向计算
+        input_array: 输入向量，维度必须等于input_size
+        '''
+        # 式2
+        self.input = input_array
+        self.output = self.activator.forward(
+            np.dot(self.W, input_array) + self.b)
+
+    def backward(self, delta_array):
+        '''
+        反向计算W和b的梯度
+        delta_array: 从上一层传递过来的误差项
+        '''
+        # 式8
+        self.delta = self.activator.backward(self.input) * np.dot(
+            self.W.T, delta_array)
+        self.W_grad = np.dot(delta_array, self.input.T)
+        self.b_grad = delta_array
+
+    def update(self, learning_rate):
+        '''
+        使用梯度下降算法更新权重
+        '''
+        self.W += learning_rate * self.W_grad
+        self.b += learning_rate * self.b_grad
+
+# Sigmoid激活函数类
+class SigmoidActivator(object):
+    def forward(self, weighted_input):
+        return 1.0 / (1.0 + np.exp(-weighted_input))
+
+    def backward(self, output):
+        return output * (1 - output)
+
+
+# 神经网络类
+# class Network(object):
+#     def __init__(self, layers):
+#         '''
+#         构造函数
+#         '''
+#         self.layers = []
+#         for i in range(len(layers) - 1):
+#             self.layers.append(
+#                 FullConnectedLayer(
+#                     layers[i], layers[i+1],
+#                     SigmoidActivator()
+#                 )
+#             )
+#
+#     def predict(self, sample):
+#         '''
+#         使用神经网络实现预测
+#         sample: 输入样本
+#         '''
+#         output = sample
+#         for layer in self.layers:
+#             layer.forward(output)
+#             output = layer.output
+#         return output
+#
+#     def train(self, labels, data_set, rate, epoch):
+#         '''
+#         训练函数
+#         labels: 样本标签
+#         data_set: 输入样本
+#         rate: 学习速率
+#         epoch: 训练轮数
+#         '''
+#         for i in range(epoch):
+#             for d in range(len(data_set)):
+#                 self.train_one_sample(labels[d],
+#                     data_set[d], rate)
+#
+#     def train_one_sample(self, label, sample, rate):
+#         self.predict(sample)
+#         self.calc_gradient(label)
+#         self.update_weight(rate)
+#
+#     def calc_gradient(self, label):
+#         delta = self.layers[-1].activator.backward(
+#             self.layers[-1].output
+#         ) * (label - self.layers[-1].output)
+#         for layer in self.layers[::-1]:
+#             layer.backward(delta)
+#             delta = layer.delta
+#         return delta
+#
+#     def update_weight(self, rate):
+#         for layer in self.layers:
+#             layer.update(rate)
+
+
+def get_result(vec):
+    max_value_index = 0
+    max_value = 0
+    for i in range(len(vec)):
+        if vec[i] > max_value:
+            max_value = vec[i]
+            max_value_index = i
+    return max_value_index
+
+def evaluate(network, test_data_set, test_labels):
+    error = 0
+    total = len(test_data_set)
+
+    for i in range(total):
+        label = get_result(test_labels[i])
+        predict = get_result(network.predict(test_data_set[i]))
+        if label != predict:
+            error += 1
+    return float(error) / float(total)
+
+def train_and_evaluate():
+    last_error_ratio = 1.0
+    epoch = 0
+    train_data_set, train_labels = DataLoader.get_training_data_set()
+    test_data_set, test_labels = DataLoader.get_test_data_set()
+    network = Network([784, 300, 10])
+    print("start trainning")
+    while True:
+        epoch += 1
+        network.train(train_labels, train_data_set, 0.3, 1)
+        print ('%s epoch %d finished' % (datetime.now(), epoch))
+        if epoch % 10 == 0:
+            error_ratio = evaluate(network, test_data_set, test_labels)
+            print ('%s after epoch %d, error ratio is %f' % (datetime.now(), epoch, error_ratio))
+            if error_ratio > last_error_ratio:
+                break
+            else:
+                last_error_ratio = error_ratio
+
+
+if __name__ == '__main__':
+    train_and_evaluate()
---
+++ utils/DataLoader.py
@@ -0,0 +1,127 @@
+#!usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+# #  FileName    : 
+# #  Author      : Administrator
+# #  Description : 
+# #  Time        : 2017/5/2
+
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import struct
+from bp import *
+from datetime import datetime
+import sys
+import os
+
+# Data loader
+class Loader(object):
+    def __init__(self, path, count):
+        '''
+        Init data loader
+        path: file path
+        count: number of file
+        '''
+        self.path = path
+        self.count = count
+
+    def get_file_content(self):
+        '''
+        Read file content
+        '''
+        print(self.path)
+        f = open(self.path, 'rb')
+        content = f.read()
+        f.close()
+        return content
+
+    def to_int(self, byte):
+        '''
+        Change unsigned byte to int
+        '''
+        return struct.unpack('b', str(byte).encode("utf-8"))[0]
+
+
+# Image data loader
+class ImageLoader(Loader):
+    def get_picture(self, content, index):
+        '''
+        Get image from file
+        '''
+        start = index * 28 * 28 + 16
+        picture = []
+        for i in range(28):
+            picture.append([])
+            for j in range(28):
+                picture[i].append(
+                    content[start + i * 28 + j])
+                    # self.to_int(content[start + i * 28 + j]))
+        return picture
+
+    def get_one_sample(self, picture):
+        '''
+        Change image to input sample vector
+        '''
+        sample = []
+        for i in range(28):
+            for j in range(28):
+                sample.append(picture[i][j])
+        return sample
+
+    def load(self):
+        '''
+        Load image data to get image vector
+        '''
+        content = self.get_file_content()
+        data_set = []
+        for index in range(self.count):
+            data_set.append(
+                self.get_one_sample(
+                    self.get_picture(content, index)))
+        return data_set
+
+
+# Label data loader
+class LabelLoader(Loader):
+    def load(self):
+        '''
+        Load label data to get label vector
+        '''
+        content = self.get_file_content()
+        labels = []
+        for index in range(self.count):
+            labels.append(self.norm(content[index + 8]))
+        return labels
+
+    def norm(self, label):
+        '''
+        Change label to 10-D vector
+        '''
+        label_vec = []
+        label_value = self.to_int(label)
+        for i in range(10):
+            if i == label_value:
+                label_vec.append(0.9)
+            else:
+                label_vec.append(0.1)
+        return label_vec
+
+
+def get_training_data_set():
+    '''
+    Get training data set
+    '''
+    # image_loader = ImageLoader(Loader(os.path.join(os.path.pardir(os.getcwd()), "dataset\\train-images.idx3-ubyte")), 60000)
+    # label_loader = LabelLoader(Loader(os.path.join(os.path.pardir(os.getcwd()), "dataset\\train-labels.idx1-ubyte")), 60000)
+    image_loader = ImageLoader("train-images.idx3-ubyte", 20)  #60000
+    label_loader = LabelLoader("train-labels.idx1-ubyte", 20)  #60000
+    return image_loader.load(), label_loader.load()
+
+
+def get_test_data_set():
+    '''
+    Get test data set
+    '''
+    image_loader = ImageLoader('t10k-images.idx3-ubyte', 10)
+    label_loader = LabelLoader('t10k-labels.idx1-ubyte', 10)
+    return image_loader.load(), label_loader.load()
